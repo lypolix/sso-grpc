464ed49512b136a570935737c1a7f3223c8d92f1
Polina <plusik-006@mail.ru>
2025-08-27 23:25:07 +0300

    First commit: реализация конфига и запуск приложения

---
+++ internal/app/app.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"log/slog"
+	"time"
+
+	grpcapp "sso-auth/internal/app/grpc"
+)
+
+
+
+type App struct {
+	GRPCSrv *grpcapp.App
+}
+
+func New (log *slog.Logger, grpcPort int, storagePath string, tokenTTL time.Duration) * App {
+
+	
+	grpcApp := grpcapp.New(log, grpcPort)
+
+	return &App {
+		GRPCSrv: grpcApp,
+	}
+}
---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"context"
+
+	ssov1 "github.com/lypolix/protos_sso/gen/go/sso"
+	"google.golang.org/grpc"
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+}
+
+func Register(gRPC *grpc.Server){
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+}
+
+func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error){
+	return  &ssov1.LoginResponse{
+		Token: "token1234",
+	}, nil
+}
+
+func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error){
+	panic("implenent me")
+}
+
+func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error){
+	panic("implenent me")
+}
